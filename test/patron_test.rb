c2f70f665e055fcf513ca4202cef8765dcf93110
Saryn Mooney <smooney15@my.whitworth.edu>
2020-05-27 09:18:27 -0600

    patron tests created and passed

---
+++ test/patron_test.rb
@@ -0,0 +1,33 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/patron'
+require './lib/exhibit'
+require 'pry'
+
+
+class PatronTest < MiniTest::Test
+
+  def test_it_exists
+    patron_1 = Patron.new("Bob", 20)
+    assert_instance_of Patron, patron_1
+  end
+
+  def test_it_has_attributes
+    patron_1 = Patron.new("Bob", 20)
+    assert_equal "Bob", patron_1.name
+    assert_equal 20, patron_1.spending_money
+  end
+
+  def test_it_starts_with_no_interests
+    patron_1 = Patron.new("Bob", 20)
+    assert_equal [], patron_1.interests
+  end
+
+  def test_it_can_add_interests
+    patron_1 = Patron.new("Bob", 20)
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_1.add_interest("Gems and Minerals")
+    assert_equal ["Dead Sea Scrolls", "Gems and Minerals"], patron_1.interests
+  end
+
+end
